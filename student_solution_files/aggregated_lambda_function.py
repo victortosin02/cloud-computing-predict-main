ea64c5f0d07bedd782582d9f72b25ac62a7cbc2f
victortosin02 <victor.oladejo@cloudware.ng>
2023-05-05 17:56:34 +0100

    aggregated lambda

--- student_solution_files/aggregated_lambda_function.py
+++ student_solution_files/aggregated_lambda_function.py
@@ -22,17 +22,182 @@ import numpy as np # Array manipulation
 # <<< You will need to add additional libraries to complete this script >>> 
 
 # ** Insert key phrases function **
-# --- Insert your code here ---
+def key_phrase_finder(list_of_important_phrases, list_of_extracted_phrases):
+    listing = []
+    PhraseChecker = None
+
+    res = str(list_of_extracted_phrases).split()
+
+    for important_word in list_of_important_phrases:
+        names = res
+        names2 = [word for word in names if important_word in word]
+        isnot_empty = np.array(names2).size > 0
+        
+        if isnot_empty == True:
+            listing = np.append(listing, names2)
+            
+        else:
+            listing = listing
+            
+    if np.array(listing).size > 0:
+        PhraseChecker = True
+        
+    else:
+        PhraseChecker = False
+    
+    return listing, PhraseChecker
+    
+    
 
 # -----------------------------
 
 # ** Insert sentiment extraction function **
-# --- Insert your code here ---
+def find_max_sentiment(Comprehend_Sentiment_Output):
+    
+    sentiment_score = 0
+
+    if Comprehend_Sentiment_Output['Sentiment'] == 'POSITIVE':
+        sentiment_score = Comprehend_Sentiment_Output['SentimentScore']['Positive']
+
+    elif Comprehend_Sentiment_Output['Sentiment'] == 'NEGATIVE':
+        sentiment_score = Comprehend_Sentiment_Output['SentimentScore']['Negative']
+
+    elif Comprehend_Sentiment_Output['Sentiment'] == 'NEUTRAL':
+        sentiment_score = Comprehend_Sentiment_Output['SentimentScore']['Neutral']
+
+    else:
+        sentiment_score = Comprehend_Sentiment_Output['SentimentScore']['Mixed']
+
+    print(sentiment_score, Comprehend_Sentiment_Output['Sentiment'])
+    
+    return Comprehend_Sentiment_Output['Sentiment'], sentiment_score
  
 # -----------------------------
 
 # ** Insert email responses function **
-# --- Insert your code here ---
+def email_response(name, critical_phrase_list, list_of_extracted_phrases, AWS_Comprehend_Sentiment_Dump):
+    
+    # Function Constants
+    SENDER_NAME = 'Place your name here'
+    
+    # --- Check for the sentiment of the message and find dominant sentiment score ---
+    Sentiment_finder = find_max_sentiment(AWS_Comprehend_Sentiment_Dump)
+    overwhelming_sentiment = Sentiment_finder[0]
+    overwhelming_sentiment_score = Sentiment_finder[1]
+    
+    # --- Check for article critical phrases ---
+    Phrase_Matcher_Article = key_phrase_finder(critical_phrase_list,  list_of_extracted_phrases)
+    Matched_Phrases_Article = Phrase_Matcher_Article[0]
+    Matched_Phrases_Checker_Article = Phrase_Matcher_Article[1]
+    
+    # --- Check for project phrases ---
+    Phrase_Matcher_Project = key_phrase_finder(['github', 'git', 'Git', 
+                                                'GitHub', 'projects', 
+                                                'portfolio', 'Portfolio'],  
+                                                list_of_extracted_phrases)
+    Matched_Phrases_Project = Phrase_Matcher_Project[0]
+    Matched_Phrases_Checker_Project = Phrase_Matcher_Project[1]
+    
+    # --- Check for C.V phrases ---
+    Phrase_Matcher_CV = key_phrase_finder(['C.V', 'resume', 'Curriculum Vitae',
+                                           'Resume', 'CV'],  
+                                           list_of_extracted_phrases)
+    Matched_Phrases_CV = Phrase_Matcher_CV[0]
+    Matched_Phrases_Checker_CV = Phrase_Matcher_CV[1]
+    
+    # --- Generate standard responses ---
+    # === DO NOT MODIFY THIS TEXT FOR THE PURPOSE OF PREDICT ASSESSMENT ===
+    Greetings_text = f'Good day {name},'
+
+    CV_text = 'I see that you mentioned my C.V in your message. \
+               I am happy to forward you my C.V in response. \
+               If you have any other questions or C.V related queries please do get in touch. '
+
+    Project_Text = 'The projects I listed on my site only include \
+                    the ones not running in production. I have \
+                    several other projects that might interest you.'
+    
+    Article_Text = 'In your message you mentioned my blog posts and data science articles. \
+                   I have several other articles published in academic journals. \
+                   Please do let me know if you are interested - I am happy to forward them to you'
+  
+    Negative_Text = f'I see that you are unhappy in your response. \
+                    Can we please set up a session to discuss why you are not happy, \
+                    be it with the website, my personal projects or anything else. \
+                    \n\nLooking forward to our discussion. \n\nKind Regards, \n\nMy Name'
+ 
+    Neutral_Text = f'Thank you for your email. Let me know if you need any additional information.\
+                    \n\nKind Regards, \n\n{SENDER_NAME}'
+    
+    Farewell_Text = f'Thank you for your email.\n\nIf there is anything else I can assist \
+                     you with please let me know and I will set up a meeting for us to meet\
+                     in person.\n\nKind Regards, \n\n{SENDER_NAME}'
+    # =====================================================================
+    
+    # --- Email Logic --- 
+    if overwhelming_sentiment == 'POSITIVE':
+        if  ((Matched_Phrases_Checker_CV == True) & \
+            (Matched_Phrases_Checker_Article == True) & \
+            (Matched_Phrases_Checker_Project == True)):
+            
+            mytuple = (Greetings_text, CV_text, Article_Text, Project_Text, Farewell_Text)
+            Text = "\n \n".join(mytuple)
+            
+        elif ((Matched_Phrases_Checker_CV == True) & \
+             (Matched_Phrases_Checker_Article == False) & \
+             (Matched_Phrases_Checker_Project == True)):
+            
+            mytuple = (Greetings_text, CV_text, Project_Text, Farewell_Text)
+            Text = "\n \n".join(mytuple)
+            
+        elif ((Matched_Phrases_Checker_CV == True) & \
+             (Matched_Phrases_Checker_Article == False) & \
+             (Matched_Phrases_Checker_Project == False)):
+            
+            mytuple = (Greetings_text, CV_text, Farewell_Text)
+            Text = "\n \n".join(mytuple)
+            
+        elif ((Matched_Phrases_Checker_CV == False) & \
+             (Matched_Phrases_Checker_Article == True) & \
+             (Matched_Phrases_Checker_Project == False)):
+            
+            mytuple = (Greetings_text, Article_Text, Farewell_Text)
+            Text = "\n \n".join(mytuple)       
+            
+        elif ((Matched_Phrases_Checker_CV == False) & \
+             (Matched_Phrases_Checker_Article == False) & \
+             (Matched_Phrases_Checker_Project == False)):
+
+            mytuple = (Greetings_text, Farewell_Text)
+            Text = "\n \n".join(mytuple)   
+            
+        elif ((Matched_Phrases_Checker_CV == False) & \
+             (Matched_Phrases_Checker_Article == False) & \
+             (Matched_Phrases_Checker_Project == True)):
+            
+            mytuple = (Greetings_text, Project_Text ,Farewell_Text)
+            Text = "\n \n".join(mytuple)   
+            
+        elif  ((Matched_Phrases_Checker_CV == True) & \
+              (Matched_Phrases_Checker_Article == True) & \
+              (Matched_Phrases_Checker_Project == False)):
+            
+            mytuple = (Greetings_text, CV_text, Article_Text, Farewell_Text)
+            Text = "\n \n".join(mytuple)
+            
+        else:
+            mytuple = (Greetings_text, Project_Text, Article_Text, Farewell_Text)
+            Text = "\n \n".join(mytuple)
+            
+    elif overwhelming_sentiment == 'NEGATIVE':
+            mytuple = (Greetings_text, Negative_Text)
+            Text = "\n \n".join(mytuple)
+            
+    else:
+            mytuple = (Greetings_text, Neutral_Text)
+            Text = "\n \n".join(mytuple)
+    
+    return Text
  
 # -----------------------------
 
@@ -51,7 +216,13 @@ def lambda_handler(event, context):
 
 
     # Do not change the name of this variable
-    db_response = None
+    db_response = table.put_item(Item={'ResponsesID': rid, # <--- Insert the correct variable
+                        'Name': dec_dict['name'], # <--- Insert the correct variable
+                        'Email': dec_dict['email'], # <--- Insert the correct variable
+                        'Cell': dec_dict['phone'], # <--- Insert the correct variable
+                        'Message': dec_dict['message'] # <--- Insert the correct variable
+    })
+
     # -----------------------------
     
 
@@ -80,7 +251,44 @@ def lambda_handler(event, context):
     # <<< Ensure that the response text is stored in the variable `email_text` >>> 
     # --- Insert your code here ---
     # Do not change the name of this variable
-    email_text = None 
+    email_text = email_text = 'Here is the sample mail to test my form submission to EDSA using AWS SES and Lambda Function'
+    SENDER = 'victortosin01@gmail.com'
+    RECIPIENT = 'edsa.predicts@explore-ai.net'
+    SUBJECT = f"Data Science Portfolio Project Website - Hello {dec_dict['name']}"
+    BODY_TEXT = (email_text)
+    CHARSET = "UTF-8"
+    client = boto3.client('ses')
+    try:
+        #Provide the contents of the email.
+        ses_response = client.send_email(
+            Destination={
+                'ToAddresses': [
+                    RECIPIENT,
+                    'edsa.predicts@explore-ai.net',
+                ],
+            },
+            Message={
+                'Body': {
+
+                    'Text': {
+                        'Charset': CHARSET,
+                        'Data': BODY_TEXT,
+                    },
+                },
+                'Subject': {
+                    'Charset': CHARSET,
+                    'Data': SUBJECT,
+                },
+            },
+            Source=SENDER,
+        )
+
+    # Display an error if something goes wrong.	
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+    else:
+        print("Email sent! Message ID:"),
+        print(ses_response['MessageId']) 
 
     
     # -----------------------------
